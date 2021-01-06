ec94acd2de202a4b03d43e6457f7cb240b73d160
tanahara <s10.vermouth.4869@gmail.com>
2021-01-06 22:00:55 +0900

    新規投稿機能を実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,14 @@ class ShopsController < ApplicationController
   def new
     @shop = Shop.new
   end
+
+  def create
+    Shop.create(shop_params)
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :date, :content)
+  end
 end
